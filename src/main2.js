c7b3ff3fa1c3f6449fc7e2ecd14f913f2d613e12
vivi <15961591506@163.com>
2020-04-27 00:34:17 +0800

    init

---
+++ src/main2.js
@@ -0,0 +1,9 @@
+const div = dom.find('#test')[0] // 获取对应的元素
+console.log(div);
+
+dom.style(div, 'color', 'red'); // 设置 div.style.color
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+console.log(divList);
+
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
